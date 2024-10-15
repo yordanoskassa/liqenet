a598e856aab1acb08021e2d3346ec0941c412be5
Yordanos Kassa <146584948+yordanoskassa@users.noreply.github.com>
2024-10-14 20:16:31 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,13 @@ const FormDataModel = require('./models/FormData'); // Ensure the model path is 
 const app = express();
 
 // Middleware
-app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://www.liqenet.kassa.dev',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+));
 
 // MongoDB connection
 mongoose.connect('mongodb+srv://kassay:BdyQyJWbTVH3l6K1@cluster1.gep2t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1', {
